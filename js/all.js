fc6238ec528cb61e6f59350a519eacba8148f3b1
Steve <ycsteven.tsao@gmail.com>
2020-04-29 11:10:57 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("h1").click(function (e) {
+        // $("p").fadeToggle(2000);
+        // $(".box").slideUp(2000).slideDown(2000);
+        $("p").addClass("orange");
+    });
+});
