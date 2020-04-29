6dd5d27ec0267300b4e416ab7504c5c982b0f161
Steve <ycsteven.tsao@gmail.com>
2020-04-29 12:24:29 +0800

    jQ ex.

--- js/all.js
+++ js/all.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-    $("h1").click(function (e) {
-        // $("p").fadeToggle(2000);
-        // $(".box").slideUp(2000).slideDown(2000);
-        $("p").addClass("orange");
-    });
+    // $("h1").click(function (e) {
+    //     $("p").fadeToggle(2000);
+    //     $(".box").slideUp(2000).slideDown(2000);
+    //     $("p").toggleClass("orange");
+
+    // });
+    $("body").html("<h2>tygh</h2>");
 });
